a90241b00c70355d9350c5fde40cb7cfbbedc3f3
Mlabarbara <mlabarbara@gmail.com>
2023-10-05 13:30:18 -0600

    update vars

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "pm_api_url" {
+  description = "Proxmox API"
+}
+variable "pm_api_token_id" {
+  description = "Token ID"
+}
+variable "pm_api_token_secret" {
+  description = "Token Secret"
+}
+variable "sshkeys" {
+  description = "SSH key to match ID/Secret"
+}
