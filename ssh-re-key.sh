eaa0d3e112aa7af2fc39ab0d5ee2012ace3fbbb0
Mlabarbara <mlabarbara@gmail.com>
2023-10-06 12:46:35 -0600

    tee to known hosts

--- ssh-re-key.sh
+++ ssh-re-key.sh
@@ -4,9 +4,9 @@ ssh-keygen -R 192.168.12.20
 
 for i in {111..120}; do ssh-keygen -R 192.168.12.$i; done
 
-ssh-keyscan -H 192.168.12.20
+ssh-keyscan -H 192.168.12.20 >> ~/.ssh/known_hosts
 
-for ip in {111..120}; do ssh-keyscan -H 192.168.12.$ip; done
+for ip in {111..120}; do ssh-keyscan -H 192.168.12.$ip >> ~/.ssh/known_hosts; done
 
 
 
