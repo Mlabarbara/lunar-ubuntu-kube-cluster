d170857cc2bd84588546eb67faf7e11a572fec46
Mlabarbara <mlabarbara@gmail.com>
2023-10-06 07:14:20 -0600

    change size of RAM and scsi disk

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "proxmox_vm_qemu" "kube_server" {
   disk {
     storage = "local-lvm"
     slot = 0
-    size = "5G"
+    size = "10G"
     type = "scsi"
     iothread = 1
     discard = "on"
@@ -46,24 +46,24 @@ resource "proxmox_vm_qemu" "kube_server" {
 resource "proxmox_vm_qemu" "kube_node1" {
   count = 1
   target_node = "pve"
-  clone = "ubuntu-cloud-0"
+  clone = "ubuntu-cloud-1"
   name = "kube-node-1"
   vmid = "211"
 
 
   agent = 1
   os_type = "ssd1"
-  cores = 2
-  memory = 2048
+  cores = 4
+  memory = 4096
   sockets = 1
   scsihw = "virtio-scsi-single"
   bootdisk = "scsi0"
   balloon = 0
 
   disk {
-    storage = "local-lvm"
+    storage = "ssd2"
     slot = 0
-    size = "5G"
+    size = "10G"
     type = "scsi"
     discard = "on"
     iothread = 1
@@ -78,24 +78,24 @@ resource "proxmox_vm_qemu" "kube_node1" {
 resource "proxmox_vm_qemu" "kube_node2" {
   count = 1
   target_node = "pve"
-  clone = "ubuntu-cloud-1"
+  clone = "ubuntu-cloud-2"
   name = "kube-node-2"
   vmid = "222"
 
 
   agent = 1
   os_type = "cloud-init"
-  cores = 2
-  memory = 2048
+  cores = 4
+  memory = 4096
   sockets = 1
   scsihw = "virtio-scsi-single"
   bootdisk = "scsi0"
   balloon = 0
 
   disk {
-    storage = "ssd1"
+    storage = "ssd2"
     slot = 0
-    size = "5G"
+    size = "10G"
     type = "scsi"
     discard = "on"
     iothread = 1
